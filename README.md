28058724fdff01c3e828eb1b41c447312d0f09f7
Vincent136 <13520136@std.stei.itb.ac.id>
2022-02-28 13:51:37 +0700

    finish

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Tucil-2-ConvexHull
-Tucil 2 DnC Convex Hull
+## Tucil 2 DnC Convex Hull
+## Vincent Christian Siregar 13520136
+
+# Deskripsi Program
+Program ini merupakan penyelesaian dari tugas kecil 2 IF 2211 Strategi Algoritma 2021/2022. Program dapat menemukan convex hull atau titik-titik yang termasuk titik - titik terluar dari sebuah dataset. Program akan menampilkan visualisasi convex hull dalam bentuk grafik yang dapat dilihat langsung oleh pengguna.
+
+# Requirement Program
+1. Python 3, dapat diunduh di https://www.python.org/downloads/
+2. cli (dapat menggunakan cmd, powershell, dsb)
+3. pip
+4. Pandas, dapat diunduh menggunakan ```pip install pandas``` pada cli
+5. Matplotlib, dapat diunduh menggunakan ```pip install -U matplotlib``` pada cli
+6. Sklearn, dapat diunduh menggunakan ```pip install -U scikit-learn``` pada cli
+7. Matplotlib, dapat diunduh menggunakan ```pip install pandas``` pada cli
+
+# Cara menggunakan program
+1. Unduh atau clone repository ini
+2. Masuk ke folder src pada cli
+3. jalankan py visualizer.py atau python visualizer.py
+4. ikuti instruksi pada cli pada saat program berjalan
+5. visualisasi akan muncul pada window baru.
+6. Tutup window visualisasi jika sudah puas melihat hasil convex hull.
+7. kembali ke cli dan ikuti instruksi selanjutnya.
--- src/myConvexHull.py
+++ src/myConvexHull.py
@@ -2,38 +2,37 @@ import math
 
 class ConvexHull:
     def __init__(self, bucket):
-        self.bucket = bucket
+        self.bucket = bucket.tolist()
         
-        def getVertices(bucket):
-            #ubah input menjadi list
-            array = bucket.tolist()
+        #Fungsi untuk mencari index titik yang merupakan convex hull
+        def getVertices():
 
             #mencari index letak titik paling kiri dan paling kanan
             x = []
-            for item in array:
+            for item in self.bucket:
                 x.append(item[0])
             idx_min,idx_max = getMinMax(x)
             
             #membagi titik titik menjadi s1 dan s2
             matrix = [[1 for i in range(3)] for i in range(3)]
-            matrix[0][0] = array[idx_min][0]
-            matrix[0][1] = array[idx_min][1]
-            matrix[1][0] = array[idx_max][0]
-            matrix[1][1] = array[idx_max][1]
+            matrix[0][0] = self.bucket[idx_min][0]
+            matrix[0][1] = self.bucket[idx_min][1]
+            matrix[1][0] = self.bucket[idx_max][0]
+            matrix[1][1] = self.bucket[idx_max][1]
 
             s1 = []
             s2 = []
-            for i in range(len(array)):
-                matrix[2][0] = array[i][0]
-                matrix[2][1] = array[i][1]
+            for i in range(len(self.bucket)):
+                matrix[2][0] = self.bucket[i][0]
+                matrix[2][1] = self.bucket[i][1]
                 det = getDeterminant(matrix)
                 if (det > 0):
                     s1.append(i)
                 elif (det < 0) :
                     s2.append(i)
             
-            Hull1 = RecConvexHull(idx_min,idx_max, s1, array, True)
-            Hull2 = RecConvexHull(idx_min,idx_max, s2, array, False)
+            Hull1 = RecConvexHull(idx_min,idx_max, s1, True)
+            Hull2 = RecConvexHull(idx_min,idx_max, s2, False)
             invertArray(Hull2)
 
             vertices = [idx_min]
@@ -45,19 +44,27 @@ class ConvexHull:
 
             return vertices
         
-        def getSimplices(vertices):
+        # Fungsi untuk mendapatkan pasangan titik untuk divisualisasikan
+        def getSimplices():
             simplices = []
-            for i in range(len(vertices)):
+            for i in range(len(self.vertices)):
                 s = []
-                if i != len(vertices)-1:
-                    s.append(vertices[i])
-                    s.append(vertices[i+1])
+                if i != len(self.vertices)-1:
+                    s.append(self.vertices[i])
+                    s.append(self.vertices[i+1])
                 else:
-                    s.append(vertices[i])
-                    s.append(vertices[0])
+                    s.append(self.vertices[i])
+                    s.append(self.vertices[0])
                 simplices.append(s)
             
             return simplices
+        
+        def getPoint():
+            container = []
+            for i in self.vertices:
+                container.append(self.bucket[i])
+
+            return container
 
         #fungsi untuk membalikan urutan array
         def invertArray(array):
@@ -101,33 +108,37 @@ class ConvexHull:
                     det += sign * (a * getDeterminant(m))
                     sign *= -1
                 return det
-        
-        def DividePoint(idx1, idx2, si ,bucket, isLeft):
+
+        # Fungsi untuk mencari titik yang berada di luar garis 
+        # jika isLeft True maka fungsi mengembalikan titik-titik yang berada di kiri atas garis
+        # jika isLeft False maka fungsi mengembalikan titik-titik yang berada di kanan bawah garis
+        def DividePoint(idx1, idx2, si, isLeft):
             # inisiasi matrix untuk mencari determinan
             mat = [[1 for i in range(3)] for i in range(3)]
-            mat[0][0] = bucket[idx1][0]
-            mat[0][1] = bucket[idx1][1]
-            mat[1][0] = bucket[idx2][0]
-            mat[1][1] = bucket[idx2][1]
+            mat[0][0] = self.bucket[idx1][0]
+            mat[0][1] = self.bucket[idx1][1]
+            mat[1][0] = self.bucket[idx2][0]
+            mat[1][1] = self.bucket[idx2][1]
 
             s = []
             if isLeft:
                 for i in si:
-                    mat[2][0] = bucket[int(i)][0]
-                    mat[2][1] = bucket[int(i)][1]
+                    mat[2][0] = self.bucket[int(i)][0]
+                    mat[2][1] = self.bucket[int(i)][1]
                     det = getDeterminant(mat)
                     if (det > 0):
                         s.append(i)
                 return s   
             else:
                 for i in si:
-                    mat[2][0] = bucket[int(i)][0]
-                    mat[2][1] = bucket[int(i)][1]
+                    mat[2][0] = self.bucket[int(i)][0]
+                    mat[2][1] = self.bucket[int(i)][1]
                     det = getDeterminant(mat)
                     if (det < 0):
                         s.append(i)
                 return s   
 
+        # Fungsi untuk mencari persamaan garis (ax + by + c = 0) antara 2 titik (x1,y1) dan (x2,y2)
         def PointToLine(x1,x2,y1,y2):
             distx = x2 - x1
             disty = y2 - y1
@@ -136,35 +147,41 @@ class ConvexHull:
             c = (distx*y1) - (disty*x1)
             return (a,b,c)
 
+        # Fungsi untuk mencari titik terjauh dari garis
         def Distance(x1, y1, Line):
             d = abs((Line[0] * x1 + Line[1] * y1 + Line[2])) / (math.sqrt(Line[0] * Line[0] + Line[1] * Line[1]))
             return d
 
-        def RecConvexHull(idx1, idx2, s, bucket, isLeft):
+        # Fungsi Rekursif untuk mencari convex hull
+        def RecConvexHull(idx1, idx2, s, isLeft):
             if len(s) == 0:
                 return []
             elif len(s) == 1:
                 return s
             else:
                 max = 0
-                Line = PointToLine(bucket[idx1][0],bucket[idx2][0],bucket[idx1][1],bucket[idx2][1])
+                id = -999
+                Line = PointToLine(self.bucket[idx1][0],self.bucket[idx2][0],self.bucket[idx1][1],self.bucket[idx2][1])
                 for i in s:
                     idx = int(i)
-                    distance = Distance(bucket[idx][0], bucket[idx][1], Line)
+                    distance = Distance(self.bucket[idx][0], self.bucket[idx][1], Line)
                     if (max < distance):
                         max = distance
                         id = idx
-            
-                s1 = DividePoint(idx1, id, s, bucket, isLeft)
-                arr1 = RecConvexHull(idx1, id, s1, bucket, isLeft)
-                s2 = DividePoint(id, idx2, s, bucket, isLeft)
-                arr2 = RecConvexHull(id, idx2, s2, bucket, isLeft)
+                if (id != -999):
+                    s1 = DividePoint(idx1, id, s, isLeft)
+                    arr1 = RecConvexHull(idx1, id, s1, isLeft)
+                    s2 = DividePoint(id, idx2, s, isLeft)
+                    arr2 = RecConvexHull(id, idx2, s2, isLeft)
 
-                arr1.append(id)
-                for item in arr2:
-                    arr1.append(item)
+                    arr1.append(id)
+                    for item in arr2:
+                        arr1.append(item)
 
-                return arr1
+                    return arr1
+                else:
+                    return []
 
-        self.vertices = getVertices(self.bucket)
-        self.simplices = getSimplices(self.vertices)
+        self.vertices = getVertices()
+        self.simplices = getSimplices()
+        self.point = getPoint()
